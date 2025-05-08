20ecea7b4f5f0d3fb8f53dfe73f68d70a1a0f12f
chrispydev <christianowusu44@gmail.com>
2025-05-08 14:13:59 +0000

    docs(file name): added requirements.txt

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+## exit on error
+#
+set -o errexit
+
+pip install -r requirements.txt
+
+mkdir -p staticfiles
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
+
